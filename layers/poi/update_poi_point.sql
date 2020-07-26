ea64e2ef3968499cc09ae82980f5681466f06c84
asakaida <sakaida@kasika.xyz>
2020-07-26 14:23:37 +0900

    remove unused

--- layers/poi/update_poi_point.sql
+++ layers/poi/update_poi_point.sql
@@ -2,45 +2,48 @@ DROP TRIGGER IF EXISTS trigger_flag ON osm_poi_point;
 DROP TRIGGER IF EXISTS trigger_refresh ON poi_point.updates;
 
 -- etldoc:  osm_poi_point ->  osm_poi_point
-CREATE OR REPLACE FUNCTION update_osm_poi_point() RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION update_osm_poi_point() RETURNS VOID AS
+$$
 BEGIN
-  UPDATE osm_poi_point
+    UPDATE osm_poi_point
     SET subclass = 'subway'
-    WHERE station = 'subway' and subclass='station';
+    WHERE station = 'subway'
+      and subclass = 'station';
 
-  UPDATE osm_poi_point
+    UPDATE osm_poi_point
     SET subclass = 'halt'
-    WHERE funicular = 'yes' and subclass='station';
+    WHERE funicular = 'yes'
+      and subclass = 'station';
 
-  UPDATE osm_poi_point
-  SET tags = update_tags(tags, geometry)
-  WHERE COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL;
+--     UPDATE osm_poi_point
+--     SET tags = update_tags(tags, geometry)
+--     WHERE COALESCE(tags -> 'name:latin', tags -> 'name:nonlatin', tags -> 'name_int') IS NULL;
 
 END;
 $$ LANGUAGE plpgsql;
 
 SELECT update_osm_poi_point();
 
-CREATE OR REPLACE FUNCTION update_osm_poi_point_agg() RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION update_osm_poi_point_agg() RETURNS VOID AS
+$$
 BEGIN
-  UPDATE osm_poi_point p
-  SET agg_stop = CASE
-      WHEN p.subclass IN ('bus_stop', 'bus_station', 'tram_stop', 'subway')
-        THEN 1
-      ELSE NULL
-  END;
+    UPDATE osm_poi_point p
+    SET agg_stop = CASE
+                       WHEN p.subclass IN ('bus_stop', 'bus_station', 'tram_stop', 'subway')
+                           THEN 1
+                       ELSE NULL
+        END;
 
-  UPDATE osm_poi_point p
+    UPDATE osm_poi_point p
     SET agg_stop = (
-      CASE
-        WHEN p.subclass IN ('bus_stop', 'bus_station', 'tram_stop', 'subway')
-            AND r.rk IS NULL OR r.rk = 1
-          THEN 1
-        ELSE NULL
-      END)
+        CASE
+            WHEN p.subclass IN ('bus_stop', 'bus_station', 'tram_stop', 'subway')
+                     AND r.rk IS NULL OR r.rk = 1
+                THEN 1
+            ELSE NULL
+            END)
     FROM osm_poi_stop_rank r
-    WHERE p.osm_id = r.osm_id
-  ;
+    WHERE p.osm_id = r.osm_id;
 
 END;
 $$ LANGUAGE plpgsql;
@@ -49,35 +52,44 @@ $$ LANGUAGE plpgsql;
 
 CREATE SCHEMA IF NOT EXISTS poi_point;
 
-CREATE TABLE IF NOT EXISTS poi_point.updates(id serial primary key, t text, unique (t));
-CREATE OR REPLACE FUNCTION poi_point.flag() RETURNS trigger AS $$
+CREATE TABLE IF NOT EXISTS poi_point.updates
+(
+    id serial primary key,
+    t  text,
+    unique (t)
+);
+CREATE OR REPLACE FUNCTION poi_point.flag() RETURNS trigger AS
+$$
 BEGIN
-    INSERT INTO poi_point.updates(t) VALUES ('y')  ON CONFLICT(t) DO NOTHING;
+    INSERT INTO poi_point.updates(t) VALUES ('y') ON CONFLICT(t) DO NOTHING;
     RETURN null;
 END;
 $$ language plpgsql;
 
 CREATE OR REPLACE FUNCTION poi_point.refresh() RETURNS trigger AS
-  $BODY$
-  BEGIN
+$BODY$
+BEGIN
     RAISE LOG 'Refresh poi_point';
     PERFORM update_osm_poi_point();
     REFRESH MATERIALIZED VIEW osm_poi_stop_centroid;
     REFRESH MATERIALIZED VIEW osm_poi_stop_rank;
+--     TODO agg_stopを使用しない可能性が高いため、次のものは不要かもしれない
     PERFORM update_osm_poi_point_agg();
     DELETE FROM poi_point.updates;
     RETURN null;
-  END;
-  $BODY$
-language plpgsql;
+END;
+$BODY$
+    language plpgsql;
 
 CREATE TRIGGER trigger_flag
-    AFTER INSERT OR UPDATE OR DELETE ON osm_poi_point
+    AFTER INSERT OR UPDATE OR DELETE
+    ON osm_poi_point
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE poi_point.flag();
+EXECUTE PROCEDURE poi_point.flag();
 
 CREATE CONSTRAINT TRIGGER trigger_refresh
-    AFTER INSERT ON poi_point.updates
+    AFTER INSERT
+    ON poi_point.updates
     INITIALLY DEFERRED
     FOR EACH ROW
-    EXECUTE PROCEDURE poi_point.refresh();
+EXECUTE PROCEDURE poi_point.refresh();
--- layers/poi/update_poi_polygon.sql
+++ layers/poi/update_poi_polygon.sql
@@ -3,29 +3,33 @@ DROP TRIGGER IF EXISTS trigger_refresh ON poi_polygon.updates;
 
 -- etldoc:  osm_poi_polygon ->  osm_poi_polygon
 
-CREATE OR REPLACE FUNCTION update_poi_polygon() RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION update_poi_polygon() RETURNS VOID AS
+$$
 BEGIN
-  UPDATE osm_poi_polygon
-  SET geometry =
-           CASE WHEN ST_NPoints(ST_ConvexHull(geometry))=ST_NPoints(geometry)
-           THEN ST_Centroid(geometry)
-           ELSE ST_PointOnSurface(geometry)
-    END
-  WHERE ST_GeometryType(geometry) <> 'ST_Point';
+    UPDATE osm_poi_polygon
+    SET geometry =
+            CASE
+                WHEN ST_NPoints(ST_ConvexHull(geometry)) = ST_NPoints(geometry)
+                    THEN ST_Centroid(geometry)
+                ELSE ST_PointOnSurface(geometry)
+                END
+    WHERE ST_GeometryType(geometry) <> 'ST_Point';
 
-  UPDATE osm_poi_polygon
-  SET subclass = 'subway'
-  WHERE station = 'subway' and subclass='station';
+    UPDATE osm_poi_polygon
+    SET subclass = 'subway'
+    WHERE station = 'subway'
+      and subclass = 'station';
 
-  UPDATE osm_poi_polygon
+    UPDATE osm_poi_polygon
     SET subclass = 'halt'
-    WHERE funicular = 'yes' and subclass='station';
+    WHERE funicular = 'yes'
+      and subclass = 'station';
 
-  UPDATE osm_poi_polygon
-  SET tags = update_tags(tags, geometry)
-  WHERE COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL;
+--     UPDATE osm_poi_polygon
+--     SET tags = update_tags(tags, geometry)
+--     WHERE COALESCE(tags -> 'name:latin', tags -> 'name:nonlatin', tags -> 'name_int') IS NULL;
 
-  ANALYZE osm_poi_polygon;
+    ANALYZE osm_poi_polygon;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -35,32 +39,40 @@ SELECT update_poi_polygon();
 
 CREATE SCHEMA IF NOT EXISTS poi_polygon;
 
-CREATE TABLE IF NOT EXISTS poi_polygon.updates(id serial primary key, t text, unique (t));
-CREATE OR REPLACE FUNCTION poi_polygon.flag() RETURNS trigger AS $$
+CREATE TABLE IF NOT EXISTS poi_polygon.updates
+(
+    id serial primary key,
+    t  text,
+    unique (t)
+);
+CREATE OR REPLACE FUNCTION poi_polygon.flag() RETURNS trigger AS
+$$
 BEGIN
-    INSERT INTO poi_polygon.updates(t) VALUES ('y')  ON CONFLICT(t) DO NOTHING;
+    INSERT INTO poi_polygon.updates(t) VALUES ('y') ON CONFLICT(t) DO NOTHING;
     RETURN null;
 END;
 $$ language plpgsql;
 
 CREATE OR REPLACE FUNCTION poi_polygon.refresh() RETURNS trigger AS
-  $BODY$
-  BEGIN
+$BODY$
+BEGIN
     RAISE LOG 'Refresh poi_polygon';
     PERFORM update_poi_polygon();
     DELETE FROM poi_polygon.updates;
     RETURN null;
-  END;
-  $BODY$
-language plpgsql;
+END;
+$BODY$
+    language plpgsql;
 
 CREATE TRIGGER trigger_flag
-    AFTER INSERT OR UPDATE OR DELETE ON osm_poi_polygon
+    AFTER INSERT OR UPDATE OR DELETE
+    ON osm_poi_polygon
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE poi_polygon.flag();
+EXECUTE PROCEDURE poi_polygon.flag();
 
 CREATE CONSTRAINT TRIGGER trigger_refresh
-    AFTER INSERT ON poi_polygon.updates
+    AFTER INSERT
+    ON poi_polygon.updates
     INITIALLY DEFERRED
     FOR EACH ROW
-    EXECUTE PROCEDURE poi_polygon.refresh();
+EXECUTE PROCEDURE poi_polygon.refresh();
